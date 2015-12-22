a0e25fccf43f29ee20a975de6d9163b7bb9f7d1e
sfhati <sfhati.com@gmail.com>
2015-12-22 07:07:40 +0200

    html parser

--- plugin/___core/footer.php
+++ plugin/___core/footer.php
@@ -1,138 +1,131 @@
 <?php
-/*
-function html2a ( $html ) {
-  if ( !preg_match_all( '
-@
-\<\s*?(\w+)((?:\b(?:\'[^\']*\'|"[^"]*"|[^\>])*)?)\>
-((?:(?>[^\<]*)|(?R))*)
-\<\/\s*?\\1(?:\b[^\>]*)?\>
-|\<\s*(\w+)(\b(?:\'[^\']*\'|"[^"]*"|[^\>])*)?\/?\>
-@uxis', $html = trim($html), $m, PREG_OFFSET_CAPTURE | PREG_SET_ORDER) )
-    return $html;
-print_r($m);
-  $i = 0;
-  $ret = array();
-  foreach ($m as $set) {
-    if ( strlen( $val = trim( substr($html, $i, $set[0][1] - $i) ) ) )
-      $ret[] = $val;
-    $val = $set[1][1] < 0 
-      ? array( 'tag' => strtolower($set[4][0]) )
-      : array( 'tag' => strtolower($set[1][0]), 'val' => html2a($set[3][0]) );
-    
-   print_r($ret) ;
-  
-    $ret[] = $val;
-    $i = $set[0][1]+strlen( $set[0][0] );
-  }
-  $l = strlen($html);
-  if ( $i < $l )
-    if ( strlen( $val = trim( substr( $html, $i, $l - $i ) ) ) )
-      $ret[] = $val;
-  return $ret;
-}
-?>
+$html = file_get_contents(TEMPLATE_PATH . 'admin/test.inc');
 
-Now let's try it with this example: (there are some really nasty xhtml compliant bugs, but ... we shouldn't worry)
+$xx = (get_tag($html));
+//print_r($xx);
+$yy = get_tag($xx['html']);
+print_r($yy);
 
-<?php
-$html = <<<EOT
-<b   ddda='1' c="5" x="5"/>
-        
-   bassam 
-        <b d=5>coco</b>
-   <c a=3>rasha</c>
-   </b>
-        
-EOT;
+function get_tag($html) {
+    $find = "/<lezaz:/";
+    preg_match($find, $html, $tagpos, PREG_OFFSET_CAPTURE);
+    $tagpos = $tagpos[0][1];
+    if (!$tagpos)
+        return '';
+    $html = preg_replace($find, '<xxxxx:', $html, 1);
+    $startfrom = ($tagpos + 7);
+    $i = $startfrom;
+    $spacechr = array(' ', "\n", "\r", "\t");
+    while ($x <= 10) {
 
-$a = html2a($html);
-//now we will make some neat html out of it
-echo a2html($a);
+        $chr = substr($html, $i, 1);
+        //echo " ($chr) ";
+        if (!$tag) {
+            if (in_array($chr, $spacechr)) { //get tag name
+                $tag = substr($html, $startfrom, ($i - $startfrom));
+                $attr_array['tag'] = $tag;
+                $attrposstart = $i;
+                $attr_name = 0;
+                $agnore = 0;
+                //return get_tag($html, $i);
+            }
+        }
 
-function a2html ( $a, $in = "" ) {
-  if ( is_array($a) ) {
-    $s = "";
-    foreach ($a as $t)
-      if ( is_array($t) ) {
-        $attrs=""; 
-        if ( isset($t['attr']) )
-          foreach( $t['attr'] as $k => $v )
-            $attrs.=" ${k}=".( strpos( $v, '"' )!==false ? "'$v'" : "\"$v\"" );
-        $s.= $in."<".$t['tag'].$attrs.( isset( $t['val'] ) ? ">\n".a2html( $t['val'], $in."  " ).$in."</".$t['tag'] : "/" ).">\n";
-      } else
-        $s.= $in.$t."\n";
-  } else {
-    $s = empty($a) ? "" : $in.$a."\n";
-  }
-  return $s;
-}
-<lezaz:block inside="div#id.class1>.class>#id>span.li"
-exit();
-?>
- * 
-$html=">222x.form>#myform>div#id.class1>.class>#id>span.li";
-foreach(explode('>', $html) as $element){
-preg_match_all( '/.([^\W])+/', $element, $m) ;
 
-    if(!strpos('XX'. $m[0][0],'#') && !strpos('XX'. $m[0][0],'.')) {
-        $x='<'.$m[0][0];
-                $y='</'.$m[0][0].'>';
 
-    }else if(strpos('XX'. $m[0][0],'#')){
-        $x='<div id="'. str_replace('#','',$m[0][0]).'" ';
-        $y='</div>';
-        }else if(strpos('XX'. $m[0][0],'.')){
-            $x='<div id="'. str_replace('.','',$m[0][0]).'" ';
-            $y='</div>';
+        if ($startattrchr && $startattrchrpos) {
+            $setval = 0;
+            if ($startattrchr == ' ') {
+                if (in_array($chr, $spacechr)) {
+                    $setval = 1;
+                }
+            } else if ($chr == $startattrchr) {
+                if (substr($html, $i - 1, 1) != "\\") {
+                    $setval = 1;
+                }
+            }
+            if ($setval == 1) {
+                $attr_value = substr($html, $startattrchrpos + 1, ($i - $startattrchrpos - 1));
+                $attr_array['value'][] = $attr_value;
+                $attrposstart = $i + 1;
+                $attr_name = '';
+                $startattrvalue = 0;
+                $startattrchr = 0;
+                $startattrchrpos = 0;
+                $chrhere = 0;
+                $agnore = 0;
+            }
+        } else if ($attr_name && !$startattrchr) {
+            if (in_array($chr, $spacechr)) {
+                
+            } elseif ($chr == '"' || $chr == "'") {
+                $startattrchrpos = $i;
+                $startattrchr = $chr;
+            } else {
+                $startattrchrpos = $i;
+                $startattrchr = ' ';
+            }
+        } else if ($attrposstart && !$attr_name) {
+            if ($chr == ">") { // close tag!
+                if (substr($html, $i - 1, 1) == "/") { // end tag code                    
+                    $htmlreplace = substr($html, ($startfrom - 7), (($i - ($startfrom - 7))) + 1);
+                    $attr_array['htmltag'] = $htmlreplace;
+                    $attr_array['html'] = $html;
+                    return $attr_array;
+                } else { // innerHTML tag 
+                    preg_match_all("/<\/lezaz:" . trim($tag) . ">/", $html, $closetagpos, PREG_OFFSET_CAPTURE);
+                    $findinside = 0;
+                    foreach ($closetagpos[0] as $closet) {
+                        $htmlreplace = substr($html, ($startfrom - 7), (($closet[1] - ($startfrom - 7))) + (strlen($closet[0])));
+                        preg_match_all("/<lezaz:" . trim($tag) . "/", $htmlreplace, $opentagposx, PREG_OFFSET_CAPTURE);
+                        preg_match_all("/<\/lezaz:" . trim($tag) . ">/", $htmlreplace, $closetagposx, PREG_OFFSET_CAPTURE);
+                        $xopen = (1 + count($opentagposx[0])) . '|';
+                        $xclose = count($closetagposx[0]);
+                        if ($xopen == $xclose) {
+                            $attr_array['htmltag'] = $htmlreplace;
+                            $attr_array['html'] = $html;
+                            return $attr_array;
+                        }
+                    }
+                    return $attr_array;
+                }
+            }
+            if (in_array($chr, $spacechr)) {
+                if ($chrhere) {
+                    $agnore++;
+                } else {
+                    $attrposstart = $i + 1;
+                }
+            } elseif ($chr == '=') {
+                $attr_name = substr($html, $attrposstart, ($i - $attrposstart));
+                if (trim($attr_name)) {
+                    $attr_array['name'][] = $attr_name;
+                }
+            } else {
+                $chrhere = 1;
+                if ($agnore) {
+                    $attr_name = substr($html, $attrposstart, ($i - $attrposstart - $agnore));
+                    if (trim($attr_name)) {
+                        $attr_array['name'][] = $attr_name;
+                        $attr_array['value'][] = '';
+                        $attrposstart = $i;
+                        $attr_name = 0;
+                        $chrhere = 0;
+                        $agnore = 0;
+                    }
+                }
+            }
+        }
 
+
+
+        if ($chr == Null)
+            return $attr_array;
+        $i++;
     }
-    array_shift($m[0]);
- foreach($m[0] as $tag){  
-   if(strpos('XX'. $tag,'#')){
-        $x.=' id="'. str_replace('#','',$tag).'" ';
-        }else if(strpos('XX'. $tag,'.')){
-            $x.=' class="'. str_replace('.','',$tag).'" ';
-    
-    }  
-}
-$x.='>';
-$f[0][]=$x;
-$f[1][]=$y;
-
-}
-rsort($f[1]);
-echo implode("\n", $f[0]).'Bassam'.implode("\n", $f[1]);
-$x='';*/
-
-
-$html=  file_get_contents(TEMPLATE_PATH.'admin/content/setting.inc');
-$html=get_tags($html,0);
-
-
-function get_tags($html,$x){
-    global $lezaz;
-$element1= $lezaz->string->between('<lezaz:','/>',$html);
-$element2= $lezaz->string->between('<lezaz:','</lezaz:',$html);
-$posel1=(strpos($html,$element1)+strlen($element1));
-$posel2=(strpos($html,$element2)+strlen($element2));
-echo "$posel1 && !$posel2\n";
-if($x=200) return "\n\n====================$html================\n\n";
-if((strpos($html,$element1)+strlen($element1)) < (strpos($html,$element2)+strlen($element2))){
-    $tag=  explode(' ', $element1);
-    $tag= $tag[0];
-    $element = '<lezaz:'.$element1.'/>';
-}else{
-    $tag=  explode(' ', $element2);
-    $tag= $tag[0];
-    $element = '<lezaz:'.$element2.'</lezaz:'.$tag.'>';
-}
-$html= str_replace ($element, $tag, $html);
-$x++;
-
-return get_tags($html,$x);
-
 }
 
 
-echo "\n\n-----------------$html---------------------\n\n";
+
+echo "\n\n------------------Orginal HTML Code--------------------\n\n$html";
 exit();
